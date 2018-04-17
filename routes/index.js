8d4da62f9e3ccfdd39c200daa8609314d3194b08
Septian A Tama <sfirst.tama@gmail.com>
2018-04-17 10:38:47 +0700

    add 2017

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Platinum Foxes!' });
+  res.render('index', { title: 'Hello Platinum Foxes 2017!' });
 });
 
 module.exports = router;
