bb03709817ecb11f01225eaa1a7223b2cda5dab3
Septian A Tama <sfirst.tama@gmail.com>
2018-04-17 10:52:51 +0700

    kembali berhasil

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/hello', function(req, res, next) {
-  res.render('index', { title: 'Hello Platinum Foxes 2017!' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello Platinum Foxes!' });
 });
 
 module.exports = router;
